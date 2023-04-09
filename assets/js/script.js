3721f9a6ea19764fb6e035badf290ffe24791133
NadeemTalaat <nadeem.talaat@gmail.com>
2023-04-09 01:12:47 -0400

    Create function to get geolocation data based on city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
+function getLocation(cityName) {
+  fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a67ac493f3a4c2258e0cace5348bb483"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 
+function getWeather(cityCoords) {
+  var cityLat = cityCoords.lat;
+  var cityLon = cityCoords.lon;
+
+  fetch(
+    `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=a67ac493f3a4c2258e0cace5348bb483`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      return data;
+    });
+}
