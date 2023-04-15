03ba9d66c2f3bdc7ca41d505ef45803a64b71a9b
NadeemTalaat <nadeem.talaat@gmail.com>
2023-04-15 13:51:41 -0400

    Add function to find weather information given latitude and longitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
 function getLocation(cityName) {
   fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a67ac493f3a4c2258e0cace5348bb483"
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a67ac493f3a4c2258e0cace5348bb483&limit=1`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      return data;
     });
 }
 
-function getWeather(cityCoords) {
-  var cityLat = cityCoords.lat;
-  var cityLon = cityCoords.lon;
-
+function getWeather(cityLat, cityLon) {
   fetch(
-    `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=a67ac493f3a4c2258e0cace5348bb483`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=a67ac493f3a4c2258e0cace5348bb483`
   )
     .then(function (response) {
       return response.json();
